c0f29789bf6416b1438c920cc1e065d74068c82d
Ayomide <144939674+Ayomide780@users.noreply.github.com>
2024-04-10 20:56:28 -0600

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,31 @@
+// Script for collapsible content blocks
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
+
+// Script for collapsible sets
+var collSet = document.getElementsByClassName("collapsible-set");
+var j;
+
+for (j = 0; j < collSet.length; j++) {
+  collSet[j].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var contentSet = this.nextElementSibling;
+    if (contentSet.style.display === "block") {
+      contentSet.style.display = "none";
+    } else {
+      contentSet.style.display = "block";
+    }
+  });
+}
